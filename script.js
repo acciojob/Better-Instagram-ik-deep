90b25bcbb77a077650d49c4530cbb9d0318546d8
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2023-10-23 10:27:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("src", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var src = document.getElementById(ev.dataTransfer.getData("src"));
+    var dragId = src.id;
+    var tgt = ev.currentTarget.id
+    ev.currentTarget.id=dragId;
+    src.id=tgt;
+}
